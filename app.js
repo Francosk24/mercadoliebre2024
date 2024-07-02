ab135c23860ef0c2310db44158ad71c905e6dedd
francosk24 <francoscaglioni22@gmail.com>
2024-07-02 20:38:57 -0300

    2do commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const path = require ('path');
+const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Ajuste para definir el puerto correctamente
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'));
@@ -17,15 +19,16 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'));
 });
 
-
-
+// Ruta para manejar la solicitud POST del formulario
 app.post('/submit-form', (req, res) => {
-
+    // Aquí puedes procesar los datos del formulario
+    // Luego, redirige a la página index.html
     res.redirect('/');
-  });
+});
 
+app.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto " + PORT);
+});
 
-app.listen(3030, () => 
-    console.log("Servidor corriendo"))
     
     
